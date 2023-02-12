5b40fabb85461de4e0c7e74852d8b6aa88de389e
David Richards <35054853+ravenOSS@users.noreply.github.com>
2023-02-12 00:30:40 -0700

    Initial config for api test

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Amplify } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet, Text, View, Pressable } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { useEffect, useState } from 'react'
+import { Provider as PaperProvider, ThemeProvider } from 'react-native-paper'
+import { useTheme, DataTable } from 'react-native-paper'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { NavigationContainer } from '@react-navigation/native'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs'
+import { Button } from 'react-native'
+
+import TabNavigation from './components/TabsNavigator'
+
+Amplify.configure(awsconfig)
+
+function SignOutButton() {
+	const { signOut } = useAuthenticator()
+	return <Button title='Sign Out' onPress={signOut} />
 }
 
+const Tab = createMaterialBottomTabNavigator()
+
+function App() {
+	return (
+		<Authenticator.Provider>
+			<Authenticator loginMechanisms={['email']}>
+				<PaperProvider>
+					<SafeAreaView style={styles.container}>
+						<StatusBar style='auto' />
+						<SignOutButton style={styles.signOut} />
+					</SafeAreaView>
+					<NavigationContainer>
+						<TabNavigation />
+					</NavigationContainer>
+				</PaperProvider>
+			</Authenticator>
+		</Authenticator.Provider>
+	)
+}
+
+export default App
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: 'gray',
+		alignItems: 'center',
+		// justifyContent: 'center',
+	},
+	title: {
+		fontSize: 25,
+		fontWeight: '500',
+		padding: 5,
+		textAlign: 'center',
+	},
+	body: {
+		backgroundColor: 'gray',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	signOut: {
+		marginBottom: 10,
+		paddingBottom: 20,
+	},
+})
