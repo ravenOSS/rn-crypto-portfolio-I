2baa2865fd89bfbfe35c07cf60aaf9dca9ce73ef
David Richards <35054853+ravenOSS@users.noreply.github.com>
2023-03-19 00:12:24 -0600

    Refactor BlocktapClient

--- components/BlocktapClient.js
+++ components/BlocktapClient.js
@@ -1,40 +1,13 @@
-import {
-	ApolloProvider,
-	ApolloClient,
-	InMemoryCache,
-	createHttpLink,
-	ApolloLink,
-	gql,
-	useMutation,
-	useQuery,
-} from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-import {
-	AWS_GraphQL_API_KEY,
-	AWS_GraphQL_ENDPOINT,
-	Blocktap_API_KEY,
-	BlockTap_GraphQL_ENDPOINT,
-} from '@env'
-
-const blocktap_httpLink = createHttpLink({
-	uri: BlockTap_GraphQL_ENDPOINT,
-})
-
-const blocktap_authLink = setContext((_, { headers }) => {
-	const token = Blocktap_API_KEY
-	//! return the headers to the context so blocktap_httpLink can read them
-	return {
-		headers: {
-			...headers,
-			authorization: token ? `Bearer ${token}` : '',
-		},
-	}
-})
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import { Blocktap_API_KEY, BlockTap_GraphQL_ENDPOINT } from '@env'
 
 const BlocktapClient = new ApolloClient({
-	link: blocktap_authLink.concat(blocktap_httpLink),
+	uri: BlockTap_GraphQL_ENDPOINT,
+	//! change cache to network only
 	cache: new InMemoryCache(),
-	credentials: 'include',
+	headers: {
+		authorization: Blocktap_API_KEY,
+	},
 })
 
 export default BlocktapClient
